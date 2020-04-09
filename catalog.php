f64b2f13c0275b0a5390252e2b0636b40681dc8d
Jay Lampstack <jlampstack@gmail.com>
2020-04-08 22:13:18 -0400

    Finished section 2, video 4

--- catalog.php
+++ catalog.php
@@ -1,26 +1,43 @@
 <?php 
-$pageTitle = "Full Catalog";
 
-if(isset($_GET["cat"])) {
-	if($_GET["cat"] == "books") {
-		$pageTitle = "Books";
-		$section = "books";
-	} else if($_GET["cat"] == "movies") {
-		$pageTitle = "Movies";
-		$section = "movies";
-	} else if($_GET["cat"] == "music") {
-		$pageTitle = "Music";
-		$section = "music";
-	} else {
-		$pageTitle = "Full Catalog";
-	}
-} 
+	@include('inc/data.php');
 
-@include("./inc/header.php"); 
+	$pageTitle = "Full Catalog";
+
+	if(isset($_GET["cat"])) {
+		if($_GET["cat"] == "books") {
+			$pageTitle = "Books";
+			$section = "books";
+		} else if($_GET["cat"] == "movies") {
+			$pageTitle = "Movies";
+			$section = "movies";
+		} else if($_GET["cat"] == "music") {
+			$pageTitle = "Music";
+			$section = "music";
+		} else {
+			$pageTitle = "Full Catalog";
+		}
+	} 
+	@include("./inc/header.php"); 
 ?>
 
-<div class="section page">
-	<h1><?= $pageTitle; ?></h1>
+<div class="section catalog page">
+	<div class="wrapper">
+		<h1><?= $pageTitle; ?></h1>
+		<ul class="items">
+			<?php
+				foreach( $catalog as $item => $index ) {
+					echo "<li>
+									<a href='#'>
+										<img src=' {$index['img']} ' alt=' {$index['title']} ' />
+										<p>View Details</p>
+									</a>
+								</li>";
+				}
+				
+			?>
+		</ul>
+	</div>
 </div>
 
 <?php @include("./inc/footer.php"); ?>
---
+++ inc/functions.php
@@ -0,0 +1,10 @@
+<?php
+
+function get_item_html($id, $item) {
+  echo "<li>
+          <a href='#'>
+            <img src=' {$index['img']} ' alt=' {$index['title']} ' />
+            <p>View Details</p>
+          </a>
+        </li>";
+}
